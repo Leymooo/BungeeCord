89f30c55a953ff6b684dbf417c5d4b6a280f582c
BoomEaro <21033866+BoomEaro@users.noreply.github.com>
2022-06-13 20:18:06 +0300

    Little improve CaptchaGeneration

--- proxy/src/main/java/ru/leymooo/botfilter/captcha/CaptchaGeneration.java
+++ proxy/src/main/java/ru/leymooo/botfilter/captcha/CaptchaGeneration.java
@@ -40,108 +40,110 @@ public class CaptchaGeneration
         }
 
         generation = true;
-
-        List<Font> fonts = Arrays.asList(
-                new Font( Font.SANS_SERIF, Font.PLAIN, 50 ),
-                new Font( Font.SERIF, Font.PLAIN, 50 ),
-                new Font( Font.MONOSPACED, Font.BOLD, 50 ) );
-        Thread thread = new Thread( () ->
-        {
-            try
-            {
-                //Перед началом генерации, нужно удалить старую капчу, освободив байтовый буфер.
-                PacketUtils.captchas.clearCaptcha();
-                BungeeCord.getInstance().getLogger().log( Level.INFO, "[BotFilter] " + ( BungeeCord.getInstance().isEnabled() ? "Начата генерация капчи в фоне." : "Генерация капчи продолжится параллельно с загрузкой BungeeCord." ) );
-                ExecutorService executor = Executors.newFixedThreadPool( Runtime.getRuntime().availableProcessors(),
-                        new CaptchaThreadFactory() );
-                CaptchaPainter painter = new CaptchaPainter();
-                MapPalette.prepareColors();
-
-                int captchaCount = 900;
-                try
-                {
-                    String captchaCountString = System.getProperty( "captchaCount" );
-                    if ( captchaCountString != null && !captchaCountString.isEmpty() )
-                    {
-                        captchaCount = Integer.parseInt( captchaCountString );
-                    }
-                } catch ( Exception ignored )
-                {
-                }
-
-                if ( captchaCount <= 0 )
-                {
-                    captchaCount = 1;
-                }
-
-                List<CachedCaptcha.CaptchaHolder> holders = Collections.synchronizedList( new ArrayList<>() );
-
-                for ( int i = 1; i <= captchaCount; i++ )
-                {
-                    executor.execute( () ->
-                    {
-                        try
-                        {
-                            Random rnd = ThreadLocalRandom.current();
-                            String answer = randomAnswer( rnd );
-                            BufferedImage image = painter.draw( fonts.get( rnd.nextInt( fonts.size() ) ), randomNotWhiteColor( rnd ), answer );
-                            final CraftMapCanvas map = new CraftMapCanvas();
-                            map.drawImage( 0, 0, image );
-                            MapDataPacket packet = new MapDataPacket( 0, (byte) 0, map.getMapData() );
-                            CachedCaptcha.CaptchaHolder holder = CachedCaptcha.createCaptchaPacket( packet, answer );
-                            holders.add( holder );
-                        } catch ( Throwable e )
-                        {
-                            //Прекращаем генерацию если случилась любая ошибка
-                            e.printStackTrace();
-                            executor.shutdownNow();
-                        }
-                    } );
-                }
-
-                long start = System.currentTimeMillis();
-                ThreadPoolExecutor ex = (ThreadPoolExecutor) executor;
-                while ( ex.getActiveCount() != 0 )
-                {
-                    //Отображаем прогресс генерации только после полной загрузки банжи, чтобы логи не путались с другими важными логами при включении
-                    if ( BungeeCord.getInstance().isEnabled() )
-                    {
-                        BungeeCord.getInstance().getLogger().log( Level.INFO, "[BotFilter] Генерирую капчу [" + ( captchaCount - ex.getQueue().size() ) + "/" + captchaCount + "]" );
-                    }
-
-                    //Текущий список всех капч каждую секунду переобразуем в массив и вставляем, чтобы хоть какая та часть была доступна во время генерации
-                    PacketUtils.captchas.setCaptchas( holders.toArray( new CachedCaptcha.CaptchaHolder[0] ) );
-                    try
-                    {
-                        Thread.sleep( 1000L );
-                    } catch ( InterruptedException ex1 )
-                    {
-                        BungeeCord.getInstance().getLogger().log( Level.WARNING, "[BotFilter] Не могу сгенерировать капчу. Выключаю банджу", ex1 );
-                        System.exit( 0 );
-                        return;
-                    }
-                }
-
-                executor.shutdownNow();
-
-                //Окончательно устанавливаем оставшиеся капчи
-                PacketUtils.captchas.setCaptchas( holders.toArray( new CachedCaptcha.CaptchaHolder[0] ) );
-                System.gc();
-                BungeeCord.getInstance().getLogger().log( Level.INFO, "[BotFilter] Капча сгенерирована за {0} мс", System.currentTimeMillis() - start );
-            } catch ( Exception e )
-            {
-                e.printStackTrace();
-            } finally
-            {
-                generation = false;
-            }
-        } );
-
+        Thread thread = new Thread( CaptchaGeneration::generateCaptchas );
         thread.setName( "CaptchaGeneration-provider-thread" );
         thread.setPriority( Thread.MIN_PRIORITY );
         thread.start();
     }
+    private static void generateCaptchas()
+    {
+        try
+        {
+            List<Font> fonts = Arrays.asList(
+                    new Font( Font.SANS_SERIF, Font.PLAIN, 50 ),
+                    new Font( Font.SERIF, Font.PLAIN, 50 ),
+                    new Font( Font.MONOSPACED, Font.BOLD, 50 ) );
+            //Перед началом генерации, нужно удалить старую капчу, освободив байтовый буфер.
+            PacketUtils.captchas.clearCaptcha();
+            BungeeCord.getInstance().getLogger().log( Level.INFO, "[BotFilter] " + ( BungeeCord.getInstance().isEnabled() ? "Начата генерация капчи в фоне." : "Генерация капчи продолжится параллельно с загрузкой BungeeCord." ) );
+            ExecutorService executor = Executors.newFixedThreadPool( Runtime.getRuntime().availableProcessors(),
+                    new CaptchaThreadFactory() );
+            CaptchaPainter painter = new CaptchaPainter();
+            MapPalette.prepareColors();
 
+            int captchaCount = 900;
+            try
+            {
+                //TODO Как лучше быть? Использовать конфиг или через аргумент запуска?
+                String captchaCountString = System.getProperty( "captchaCount" );
+                if ( captchaCountString != null && !captchaCountString.isEmpty() )
+                {
+                    captchaCount = Integer.parseInt( captchaCountString );
+                }
+            } catch ( Exception ignored )
+            {
+            }
+
+            if ( captchaCount <= 0 )
+            {
+                captchaCount = 1;
+            }
+
+            List<CachedCaptcha.CaptchaHolder> holders = Collections.synchronizedList( new ArrayList<>() );
+
+            for ( int i = 1; i <= captchaCount; i++ )
+            {
+                executor.execute( () -> generate( executor, painter, fonts, holders ) );
+            }
+
+            long start = System.currentTimeMillis();
+            ThreadPoolExecutor ex = (ThreadPoolExecutor) executor;
+            while ( ex.getActiveCount() != 0 )
+            {
+                //Отображаем прогресс генерации только после полной загрузки банжи, чтобы логи не путались с другими важными логами при включении
+                if ( BungeeCord.getInstance().isEnabled() )
+                {
+                    BungeeCord.getInstance().getLogger().log( Level.INFO, "[BotFilter] Генерирую капчу [" + ( captchaCount - ex.getQueue().size() ) + "/" + captchaCount + "]" );
+                }
+
+                //Текущий список всех капч каждую секунду переобразуем в массив и вставляем, чтобы хоть какая та часть была доступна во время генерации
+                PacketUtils.captchas.setCaptchas( holders.toArray( new CachedCaptcha.CaptchaHolder[0] ) );
+                try
+                {
+                    Thread.sleep( 1000L );
+                } catch ( InterruptedException ex1 )
+                {
+                    BungeeCord.getInstance().getLogger().log( Level.WARNING, "[BotFilter] Не могу сгенерировать капчу. Выключаю банджу", ex1 );
+                    System.exit( 0 );
+                    return;
+                }
+            }
+
+            executor.shutdownNow();
+
+            //Окончательно устанавливаем оставшиеся капчи
+            PacketUtils.captchas.setCaptchas( holders.toArray( new CachedCaptcha.CaptchaHolder[0] ) );
+            System.gc();
+            BungeeCord.getInstance().getLogger().log( Level.INFO, "[BotFilter] Капча сгенерирована за {0} мс", System.currentTimeMillis() - start );
+        } catch ( Exception e )
+        {
+            e.printStackTrace();
+        } finally
+        {
+            generation = false;
+        }
+    }
+
+    private static void generate(ExecutorService executor, CaptchaPainter painter, List<Font> fonts,
+                                 List<CachedCaptcha.CaptchaHolder> holders)
+    {
+        try
+        {
+            Random rnd = ThreadLocalRandom.current();
+            String answer = randomAnswer( rnd );
+            BufferedImage image = painter.draw( fonts.get( rnd.nextInt( fonts.size() ) ), randomNotWhiteColor( rnd ), answer );
+            final CraftMapCanvas map = new CraftMapCanvas();
+            map.drawImage( 0, 0, image );
+            MapDataPacket packet = new MapDataPacket( 0, (byte) 0, map.getMapData() );
+            CachedCaptcha.CaptchaHolder holder = CachedCaptcha.createCaptchaPacket( packet, answer );
+            holders.add( holder );
+        } catch ( Throwable e )
+        {
+            //Прекращаем генерацию если случилась любая ошибка
+            e.printStackTrace();
+            executor.shutdownNow();
+        }
+    }
 
     private static Color randomNotWhiteColor(Random rnd)
     {
